4c2323bb3df6a29d2efb0e9ea7a0007435f7f555
vaibhavabhor <vaibhavabhor@gmail.com>
2013-11-19 10:43:26 -0800

    Changes from Kalyani

--- Jigsaw/SelectModeButton.java
+++ Jigsaw/SelectModeButton.java
@@ -14,21 +14,31 @@ public class SelectModeButton extends CommandButton
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     int mouseX, mouseY;
+<<<<<<< HEAD
     List modeScreen,modeBtn,Htile,modeKnb;
     public static HashMap matrix =  new HashMap();
+=======
+    List modeScreen,modeBtn,modeKnb,Htile;
+    public static HashMap matrix =  null;
+>>>>>>> df8c0f3be847a033c450b901900184d0f777f6cf
     public static Boolean modeFlag = false;
-    
+
     public void act() 
     {
         World pw = getWorld();
         modeFlag = true;
                 
         modeKnb = pw.getObjects(ModeKnob.class);
+<<<<<<< HEAD
+=======
+        Htile = pw.getObjects(HTile.class);
+
+>>>>>>> df8c0f3be847a033c450b901900184d0f777f6cf
         // Add your action code here.
-         if(Greenfoot.mousePressed(this))
+        if(Greenfoot.mousePressed(this))
         {
             //ModeKnob knob = modeKnb[0];
-            
+
             for (Object obj : modeKnb)  
             {  
                 ModeKnob knob = (ModeKnob) obj; // sub-casting  
@@ -36,9 +46,10 @@ public class SelectModeButton extends CommandButton
                 mouseY = knob.getY();
                 System.out.println("Knob cooridnates are "+ mouseX + " and "+ mouseY );
                 // change location of 'ps'  
-                
-            if (mouseY > 240
+
+                if (mouseY > 240
                 && mouseY < 260)
+<<<<<<< HEAD
             {
                 if(mouseX > 466 
                 && mouseX < 480)
@@ -55,14 +66,29 @@ public class SelectModeButton extends CommandButton
                     Actor easy = new HardLevel(pw);
                     pw.addObject(easy,890,140);
                     //navigate(pw);
+=======
+                {
+                    if(mouseX > 466 
+                    && mouseX < 480)
+                    {
+                        System.out.println("Its easy level!");
+                        navigate(pw);
+                    }  
+                    else if(mouseX > 668 
+                    && mouseX < 680)
+                    {
+                        System.out.println("Its hard level!");
+                        navigate(pw);
+                    }
+>>>>>>> df8c0f3be847a033c450b901900184d0f777f6cf
                 }
-            }
             }
         } 
     }
-    
+
     public void navigate(World pw)
     {
+<<<<<<< HEAD
       pw.removeObjects(modeScreen);
       pw.removeObjects(modeBtn);
       pw.removeObjects(Htile);
@@ -90,25 +116,52 @@ public class SelectModeButton extends CommandButton
             int cntY =0;
             int i=0;
             for (int x = 0; x < 3; x++) {  
+=======
+        matrix =  new HashMap();
+        pw.removeObjects(modeScreen);
+        pw.removeObjects(modeBtn);
+        pw.removeObjects(Htile);
+        pw.removeObjects(modeKnb);
+        GreenfootImage img = new GreenfootImage("game_screen_background.jpg");
+        pw.setBackground(img);
 
-                for (int y = 0; y < 3; y++) {  
-                    Canvas chunkgrid = new Canvas();
-                    GreenfootImage imggrid = new GreenfootImage("3x3grid.jpg");
-                    chunkgrid.setImage(imggrid); 
-                    pw.addObject(chunkgrid,105+cntX,99+cntY);
-                    cntX=cntX+121;
-                    int midX = imggrid.getWidth();
-                    int midY = imggrid.getHeight();
-                    String imageName = "img"+ i +".jpg";
-                    System.out.println("Image Name ::" + imageName+ "   mid points :: " + midX + "  " + midY);
-                    matrix.put(i,imageName);
-                    i++;
-                }
-                cntY = cntY+121;
-                cntX=0;
+        pw.addObject(new Dashboard(),890,140);
+        pw.addObject(new LeftButton(),100,535);
+        pw.addObject(new RightButton(),650,535);
+>>>>>>> df8c0f3be847a033c450b901900184d0f777f6cf
 
+        pw.addObject(new MessageBoard(),650,350);
+        pw.addObject(new ImageContainerBoard(),650,100);
+
+        pw.addObject(new LeftImage(),320,535);
+        pw.addObject(new MiddleImage(),500,535);
+        //pw.addObject(new RightImage(),480,535); 
+
+        int cntX =0;
+        int cntY =0;
+        int i=0;
+        
+        for (int x = 0; x < 3; x++) {  
+
+            for (int y = 0; y < 3; y++) {  
+                
+                Canvas chunkgrid = new Canvas();
+                GreenfootImage imggrid = new GreenfootImage("3x3grid.jpg");
+                chunkgrid.setImage(imggrid); 
+                pw.addObject(chunkgrid,105+cntX,99+cntY);
+                cntX=cntX+121;
+                int midX = imggrid.getWidth();
+                int midY = imggrid.getHeight();
+                String imageName = "img"+ i +".jpg";
+                System.out.println("Image Name ::" + imageName+ "   mid points :: " + midX + "  " + midY);
+                matrix.put(i,imageName);
+                i++;
             }
+            cntY = cntY+121;
+            cntX=0;
+
+        }
 
     }
-    
+
 }
---
+++ Jigsaw/SplitImageDrag.java
@@ -0,0 +1,84 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.*;
+
+public class SplitImageDrag extends Image
+{
+    public static ArrayList coordiantesArray = null;
+    public static ArrayList tempArray = new ArrayList();
+    public static HashMap imageCoordinateMap = new HashMap();
+    public static int counter=9;
+    public static Boolean correct = false; //this is final flag. it will true only if all images are place correctly.
+    GreenfootImage image;
+    int mouseX, mouseY, index;
+    String nameCanvas= null;
+    String name=null;
+        
+    
+    public SplitImageDrag()
+    {
+      //  GreenfootImage img = getImage();
+       // setImage(img);
+    }
+
+    public void act() 
+    {
+        World pw = getWorld();
+        
+        if(Greenfoot.mouseDragged(this))
+        { 
+            MouseInfo mouse = Greenfoot.getMouseInfo();  
+            mouseX=mouse.getX();  
+            mouseY=mouse.getY(); 
+            setLocation(mouseX, mouseY);
+           
+        }  
+
+        if(Greenfoot.mouseDragEnded(this))
+        {
+            int finalX = mouseX;
+            int finalY = mouseY;
+            coordiantesArray = new ArrayList();
+            coordiantesArray.add(finalX);
+            coordiantesArray.add(finalY);
+            coordiantesArray.add(name);
+            coordiantesArray.add(index);
+            String name = (String) Canvas.splitImagesMap.get(this.getImage());
+            System.out.println("name :: " + name + "  " + this.getImage());
+            List list = pw.getObjects(Canvas.class);
+            List matrixList = pw.getObjectsAt(finalX, finalY, Canvas.class);
+            Actor canvas = (Actor) matrixList.get(0);
+            System.out.println("Size :: " + list.size() + "  " + matrixList.size());
+            
+            for(int i =0;i<list.size();i++)
+            {
+                Actor canvasAll = (Actor) list.get(i);
+            
+                if(canvas.equals(canvasAll))
+                {
+                    System.out.println("Found");
+                    nameCanvas = (String)SelectModeButton.matrix.get(i);
+                    break;
+                }
+            
+            }
+            
+           System.out.println("kal split image name::: "+ Canvas.splitImagesMap.get(this.getImage()) + "kal canvas name:: "+ nameCanvas);
+            
+           if(nameCanvas.equals(Canvas.splitImagesMap.get(this.getImage())))
+           {
+               counter--;  
+               System.out.println("matching");
+               System.out.println("Counter :: " + counter);
+               correct = true;
+           }           
+           else
+           {
+               correct = false;
+               System.out.println("not matching");
+           }
+           // this is final flag. it will true only if all images are place correctly.
+           System.out.println("Final Flag :: " + correct);
+       }
+       
+    }  
+}
